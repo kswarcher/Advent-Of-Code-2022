ab42f8722dc4dd7b294e0144b8029f3e59a9189a
kswarcher <kswarcher@gmail.com>
2022-12-01 09:39:32 -0600

    Update and rename AOC22P1.ps1 to AOC22D1P1.ps1

--- Day 1/Part 1/AOC22D1P1.ps1
+++ Day 1/Part 1/AOC22D1P1.ps1
@@ -1,8 +1,12 @@
-﻿$elfCal = gc .\AOCD1.txt
+#Read the calories in a from a file
+$elfCal = gc .\AOCD1.txt
+
+#Initialize variables in a scope that can be used/ won't be interferred with
 $elfTotCal = @()
 [int]$oneElfCal = 0
 $mostCal = -1
 
+#Get how many calories each elf is carrying
 foreach($line in $elfCal){
     if($line -eq ""){
         $elfTotCal += $oneElfCal
@@ -13,10 +17,12 @@ foreach($line in $elfCal){
 
 }
 
+#Find the most calories carried by an elf
 foreach($elf in $elfTotCal){
     if($elf -gt $mostCal){
         $mostCal = $elf
     }
 }
 
+#Print the most calories carried by an elf
 $mostCal
