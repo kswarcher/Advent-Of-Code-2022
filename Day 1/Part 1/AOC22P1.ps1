d89f8d0e2b56ca8c4aa8e408cfa1b07bc2ea2546
kswarcher <kswarcher@gmail.com>
2022-12-01 09:23:20 -0600

    Day 1 Part 1 Code

---
+++ Day 1/Part 1/AOC22P1.ps1
@@ -0,0 +1,22 @@
+﻿$elfCal = gc .\AOCD1.txt
+$elfTotCal = @()
+[int]$oneElfCal = 0
+$mostCal = -1
+
+foreach($line in $elfCal){
+    if($line -eq ""){
+        $elfTotCal += $oneElfCal
+        $oneElfCal = 0
+    }else{
+        $oneElfCal+=$line
+    }
+
+}
+
+foreach($elf in $elfTotCal){
+    if($elf -gt $mostCal){
+        $mostCal = $elf
+    }
+}
+
+$mostCal
