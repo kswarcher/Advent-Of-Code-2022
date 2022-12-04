7b4ec212c83e3e8a212619700713a81bc7496647
kswarcher <kswarcher@gmail.com>
2022-12-04 12:04:14 -0600

    Update main.py

--- Day 4/Part 1/main.py
+++ Day 4/Part 1/main.py
@@ -3,7 +3,7 @@ def assignmentPairs():
     fileInfo = open("AOC22D4P1.txt", 'r')
     assignmentsContained = 0
 
-    # goes through the each line of the file, get sthe nubmers in each line, and finds if one range is completely in the other
+    # goes through the each line of the file, gets the numbers in each line, and finds if one range is completely in the other
     for line in fileInfo:
         num1 = int(line.split(',')[0].split('-')[0])
         num2 = int(line.split(',')[0].split('-')[1])
