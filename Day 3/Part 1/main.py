fdd5c019cbfc733938050a793c844a43801ff8cf
kswarcher <kswarcher@gmail.com>
2022-12-04 10:57:55 -0600

    Update main.py

--- Day 3/Part 1/main.py
+++ Day 3/Part 1/main.py
@@ -2,65 +2,15 @@ def ruckSacks():
     # read file in and set sum of priorities to 0
     fileInfo = open("AOC22D3P1.txt", 'r')
     totPriority = 0
-    elfNum = 1
 
-    # increments the value of the score for how many points were earned
-    for line in fileInfo:
-        found= False
-        if(elfNum == 1):
-            line1 = line
-            elfNum=2
-        elif (elfNum == 2):
-            line2 = line
-            elfNum = 3
-        elif (elfNum == 3):
-            line3 = line
-            elfNum = 1
-
-            for letter in line1:
-                if(letter in line2 and letter in line3 and not found):
-                    #print(letter, line1,line2,line3)
-                    if (ord(letter) < 91):
-                        totPriority += ord(letter) - 38
-                        found=True
-
-                    if (ord(letter) > 91):
-                        totPriority += ord(letter) - 96
-                        found=True
-
-    print(totPriority)
-"""
-        found = False
-        for letter in firstPouch:
-
-            if(letter in secondPouch and not found):
-                if(ord(letter) < 91):
-                    found = True
-                    totPriority += ord(letter)-38
-
-                if (ord(letter) > 91):
-                    found = True
-                    totPriority += ord(letter) - 96
-"""
-
-
-
-
-
-ruckSacks()
-"""
-def ruckSacks():
-    # read file in and set sum of priorities to 0
-    fileInfo = open("AOC22D3P1.txt", 'r')
-    totPriority = 0
-
-    # increments the value of the score for how many points were earned
+    # cuts the string in half, goes through the first half, and checking every letter against the second string
     for line in fileInfo:
         firstPouch = line[:len(line)//2]
         secondPouch = line[len(line)//2:]
         found = False
         for letter in firstPouch:
 
+            # if the letter has not been found it adds the priority of the letter (item) to the total priority
             if(letter in secondPouch and not found):
                 if(ord(letter) < 91):
                     found = True
@@ -73,6 +23,4 @@ def ruckSacks():
 
     print(totPriority)
 
-
-
-"""
+ruckSacks()
